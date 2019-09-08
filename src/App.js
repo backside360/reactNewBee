318b27f2e958f1532a359243fd83fecea6b98d8b
Олег Назаров <fo@MacBook-Pro-Oleg.local>
2019-09-08 17:45:46 +0300

    rename

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Navigation from './components/Nav';
-import AddContact from './components/DeleteContact';
 import Contacts from './components/ContactState';
 import 'bootstrap/dist//css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Navigation brand='Contact Manager' button='home'></Navigation>
-      <div className='container'>
+    <div className="App">
+      <Navigation brand="Contact Manager" button="home"></Navigation>
+      <div className="container">
         <Contacts></Contacts>
       </div>
     </div>
